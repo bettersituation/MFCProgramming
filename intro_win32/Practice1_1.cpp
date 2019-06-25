f1412f7b0309e768e9bcc0cb2c4a5295a4c8bd4c
bettersituation <xogml9365@gmail.com>
2019-06-26 03:53:55 +0900

    update ignore

---
+++ intro_win32/Practice1_1.cpp
@@ -0,0 +1,96 @@
+#include <Windows.h>
+#include "resource.h"
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpszCmdLine, int nCmdShow)
+{
+	static LPTSTR szAppName = (LPTSTR)"EasyText";
+	static LPTSTR szTitleName = (LPTSTR)"Practice1-1";
+
+	HWND hwnd;
+	MSG msg;
+	WNDCLASSEX WndClass;
+
+	WndClass.cbSize = sizeof(WNDCLASSEX);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = szAppName;
+	WndClass.hIconSm = 0;
+
+	RegisterClassEx(&WndClass);
+
+	hwnd = CreateWindow(
+		szAppName,
+		szTitleName,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	RECT rect;
+	PAINTSTRUCT ps;
+	LPTSTR szMsg1 = (LPTSTR)"I love WIN32 SDK!";
+	LPTSTR szMsg2 = (LPTSTR)"키보드가 눌렸습니다.";
+	LPTSTR szMsg3 = (LPTSTR)"키보드가 뗴였습니다.";
+
+	switch (message)
+	{
+	case WM_CREATE:
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		TextOut(hdc, 10, 10, szMsg1, strlen(szMsg1));
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_KEYDOWN:
+		hdc = GetDC(hwnd);
+		GetClientRect(hwnd, &rect);
+		DrawText(hdc, szMsg2, strlen(szMsg2), &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		ReleaseDC(hwnd, hdc);
+		break;
+	case WM_KEYUP:
+		hdc = GetDC(hwnd);
+		GetClientRect(hwnd, &rect);
+		DrawText(hdc, szMsg3, strlen(szMsg3), &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+		ReleaseDC(hwnd, hdc);
+		break;
+	case WM_LBUTTONDBLCLK:
+		MessageBox(hwnd, "마우스 더블클릭!", "마우스 메시지", MB_OK | MB_ICONASTERISK);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	return 0;
+}
